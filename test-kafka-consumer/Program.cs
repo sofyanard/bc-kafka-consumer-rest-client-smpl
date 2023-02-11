d6b15d478aa692c6b61b605f0b2b4eed3e369d75
Sofyan Ardianto <sofyanard@gmail.com>
2023-02-11 18:19:46 +0700

    Add project files.

---
+++ test-kafka-consumer/Program.cs
@@ -0,0 +1,136 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System;
+using System.Threading;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+using test_kafka_consumer;
+using System.Reflection.Metadata;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        using var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder
+                .AddFilter("Microsoft", LogLevel.Warning)
+                .AddFilter("System", LogLevel.Warning)
+                .AddFilter("LoggingConsoleApp.Program", LogLevel.Debug)
+                .AddConsole();
+        });
+        ILogger logger = loggerFactory.CreateLogger<Program>();
+        logger.LogInformation("Program is starting...");
+
+        var appConfig = new ConfigurationBuilder().AddJsonFile($"appsettings.json").Build();
+
+        string topic = appConfig["kafka:topic"];
+        string groupId = appConfig["kafka:group-id"];
+        string bootstrapServers = appConfig["kafka:bootstrap-server"];
+
+        var consumerConfig = new ConsumerConfig
+        {
+            GroupId = groupId,
+            BootstrapServers = bootstrapServers,
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+        logger.LogInformation($"ConsumerConfig: {JsonConvert.SerializeObject(consumerConfig)}");
+
+        CancellationTokenSource cts = new CancellationTokenSource();
+        Console.CancelKeyPress += (_, e) => {
+            e.Cancel = true; // prevent the process from terminating.
+            cts.Cancel();
+        };
+
+        using (var consumer = new ConsumerBuilder<string, string>(consumerConfig).Build())
+        {
+            logger.LogInformation($"Creating Consumer...");
+
+            consumer.Subscribe(topic);
+            logger.LogInformation($"Subscribing topic: {topic}");
+
+            try
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var cr = consumer.Consume(cts.Token);
+                        // Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+                        logger.LogInformation($"Consuming: {JsonConvert.SerializeObject(cr)}");
+                        logger.LogInformation($"Message Received: {cr.Value}");
+
+                        Thread postThread = new Thread(() => { Post(cr.Value); });
+                        postThread.Start();
+                    }
+                    catch (ConsumeException ce)
+                    {
+                        logger.LogError($"ConsumeException: {ce.Message}");
+                    }
+                }
+            }
+            catch (OperationCanceledException xe)
+            {
+                logger.LogError($"OperationCanceledException: {xe.Message}");
+            }
+            catch (Exception e)
+            {
+                logger.LogError($"Exception: {e.Message}");
+            }
+            finally
+            {
+                consumer.Close();
+            }
+        }
+
+        logger.LogInformation("Program has finished.");
+    }
+
+    private static async Task Post(string message)
+    {
+        using var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder
+                .AddFilter("Microsoft", LogLevel.Warning)
+                .AddFilter("System", LogLevel.Warning)
+                .AddFilter("LoggingConsoleApp.Program", LogLevel.Debug)
+                .AddConsole();
+        });
+        ILogger logger = loggerFactory.CreateLogger<Program>();
+
+        var appConfig = new ConfigurationBuilder().AddJsonFile($"appsettings.json").Build();
+        string host = appConfig["apisvc:url"];
+
+        HttpClient _client = new HttpClient();
+        _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        Employee employee = new Employee(1, message, message);
+
+        HttpContent body = new StringContent(JsonConvert.SerializeObject(employee), Encoding.UTF8, "application/json");
+
+        try
+        {
+            logger.LogInformation($"Sending Post...");
+            HttpResponseMessage response = await _client.PostAsync(host, body);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+
+                if (!string.IsNullOrEmpty(content))
+                {
+                    logger.LogInformation($"Get Response: {content}");
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            logger.LogError($"Exception: {e.Message}");
+        }
+    }
+}
